da58c3a6d4c237e175ff7497fc3287db586a50a1
Infinity Intellect <snavaneetharan@gmail.com>
2021-11-23 23:38:51 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+var https = require('https')
+
+
+
+const main = async() => {
+    const githubToken = core.getInput('github_token', { required: true})
+    const octokit = github.getOctokit(githubToken)
+    const context = github.context
+    const actor = github.context.actor
+    const commentStarter = core.getInput('comment_starter')
+
+    var requestOptions = {
+        host: 'zenquotes.io',
+        path: '/api/random',
+        method: 'GET'
+    }
+    var req = https.get(requestOptions, (res) => {
+        res.on('data', async (quoteData) => {
+            var quoteObject = JSON.parse(quoteData)
+            var formattedQuote = quoteObject[0].h
+            console.log(formattedQuote)
+
+            try {
+                const commentMessage = `@${actor} ${commentStarter}\nHere's an uplifting quote for the day: \n${formattedQuote}`
+                const createCommentResponse = await octokit.rest.issues.createComment({
+                    ...context.repo,
+                    issue_number: context.issue.number,
+                    body: commentMessage
+                })
+            } catch (error) {
+                core.setFailed(error.message)
+            }
+        })
+    })
+    req.on('error', (error) => {
+        console.error(error)
+    })
+    req.end()
+}
+(async() => {
+    main();
+})();
